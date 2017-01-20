625aff66ad931dd15eb585fb0dbd39dd53922ea4
Mikhail Popov <mpopov@wikimedia.org>
2017-01-20 14:28:53 -0800

    Remove bug-making JS

--- www/custom.js
+++ www/custom.js
@@ -6,14 +6,6 @@ $(function() {
       var tab = decodeURI(hash.substring(1, 100));
       $('a[data-value=\"'+tab+'\"]').click();
     }
-    
-    // Reveals the KPI dropdown menu at launch:
-    $('ul.sidebar-menu li.treeview').first().addClass('active');
-    
-    // Update the URL in the browser when a tab is clicked on:
-    $('a[href^=#shiny-tab]').click(function(){
-      window.location.hash = encodeURI($(this).attr('data-value'));
-    })
 
 });
 
